a3b25e80e91851f45d0fb77bbc6c5cf5e6ef0ee6
saurabh <mahtosaurabh1@gmail.com>
2022-06-14 23:51:15 +0530

    rgfr

---
+++ WEB/module 4 React/react prerequireties/constructiorFn.js
@@ -0,0 +1,17 @@
+function car(Name,Model,Color){
+    console.log(this);
+    this.name = Name
+    this.model = Model;
+    this.color = Color;
+
+    this.test = function(){
+        console.log(`I am driving ${this.model}`);
+    }
+}
+
+let car1 = new car('BMW','X6','White');
+console.log(car1);
+car1.test();
+
+// let car2 = new car('Mercedes','S class','Red')
+// console.log(car2);
